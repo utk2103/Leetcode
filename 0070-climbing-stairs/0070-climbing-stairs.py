8f4e4ef2d94f0a149ca310e0699a6fd849dc033b
Utkarsh Upadhyay <118432516+utk2103@users.noreply.github.com>
2024-02-09 16:28:21 +0530

    Delete 0070-climbing-stairs/0070-climbing-stairs.py

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def climbStairs(self, n: int) -> int:
-        list1 = {}
-        def dfs(i):
-            if i >= n: return 1 if i == n else 0
-            if i not in list1:
-                list1[i] = dfs(i + 1) + dfs(i + 2)
-            return list1[i]
-        return dfs(0)
