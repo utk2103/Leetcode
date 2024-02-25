02ee0f07b201649f53844252021b1f4acc93da07
Utkarsh Upadhyay <118432516+utk2103@users.noreply.github.com>
2024-02-25 23:53:26 +0530

    Update 0002-add-two-numbers.java

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -7,11 +7,10 @@ class Solution {
         while (l1 != null || l2 != null) {
             int x = l1 == null ? 0 : l1.val;
             int y = l2 == null ? 0 : l2.val;
-            // 计算对应位置上两个数相加之和 + 进位
             int sum = x + y + carr;	
-            // 计算进位的值
+        
             carr = sum / 10;
-            // 利用链表的 尾插法 插入数据
+            
             curr.next = new ListNode(sum % 10);
             curr = curr.next;
 
